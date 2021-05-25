1d34e6e48764f9f6972d51487838b8d7b03f38ff
Drew Bradley <williamandrewb@gmail.com>
2021-05-25 13:57:12 -0600

    Add useState to toggle card items.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Card } from './Card'
 import './App.css';
 
+const initialTodos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <>To Do App</>
+    <>
+    <ul>
+      <Card todo={todos[0]} />
+      <Card todo={todos[1]} />
+    </ul>
+    </>
   );
 }
 
